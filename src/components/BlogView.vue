7423a08631be5044557b6e446e8a603a7b877ccf
Igor <bla@hotmail.com>
2025-02-01 00:17:29 +0100

    popravki

--- src/components/BlogView.vue
+++ src/components/BlogView.vue
@@ -2,15 +2,15 @@
     <div>
         <button @click="showEditor = !showEditor" v-if="!showEditor">Add new blog</button>
         <BlogEditor v-else @addNewItem="addItem"/>
-        <Blogs :blogs="blogs" :is-loading="isLoading" :error="error" @deleteBlog="deleteItem"/>
+        <Blogs :blogs="blogs" :is-loading="loading" :error="error" @deleteBlog="deleteItem"/>
     </div>
 </template>
 <script setup>
 import BlogEditor from './BlogEditor.vue';
 import Blogs from './Blogs.vue';
-import { useBlogs } from '../composables/useBlogs';
+import { useBlogs } from '../composable/useBlogs';
 import { ref } from 'vue';
 
-const { blogs, isLoading, error, addItem, deleteItem } = useBlogs();
+const { blogs, loading, error, addItem, deleteItem } = useBlogs();
 const showEditor = ref(false);
 </script>
