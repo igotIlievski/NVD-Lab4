feb7ee323e1de5a1a42111ab03be0ff89eb9cd8f
Igor <bla@hotmail.com>
2025-01-31 23:41:57 +0100

    Blogs component

---
+++ src/components/Blogs.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+      <h1>Blogs</h1>
+      <div v-if="isLoading">
+        <p>Loading...</p>
+      </div>
+      <div v-else-if="error">
+        <p>{{ error }}</p>
+      </div>
+      <section v-else>
+        
+        <ul>
+          <li>
+            <article v-for="(blog, i) in filteredBlogs" :key="i">
+              <div>
+                  <img
+                  v-if="blog.heroImage"
+                  :src="`${blog.heroImage.fields.file.url}?fit=scale&w=350&h=196`"
+                  />
+                  <div>
+                  <div>
+                      {{ new Date(blog.publishDate).toDateString() }}
+                  </div>
+                  <h4>{{ blog.title }}</h4>
+                  <p>{{ blog.description }}</p>
+                  </div>
+              </div>            
+            </article>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </template>
+  <script setup>
+  import { defineProps, defineEmits, ref, watch } from "vue";
+  import { useSearch } from "../composables/useSearch";
+  
+  const props = defineProps({
+    blogs: {
+      type: Array,
+      required: true,
+    },
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+    error: {
+      type: String,
+      required: true,
+    },
+  });
+  
+  
+  const blogs = ref(props.blogs);
+  
+  watch(
+    () => props.blogs,
+    (newBlogs) => {
+      blogs.value = newBlogs;
+    }
+  );
+  
+  
+  </script>
