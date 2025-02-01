c7f8d6111dd1ff28b490e24a8ef1920c335c05a8
Igor <bla@hotmail.com>
2025-02-01 01:06:27 +0100

    opshti stilovi

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,17 @@
 <script setup>
 import BlogView from "./components/BlogView.vue";
 </script>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto 0;
+  max-width: 800px;
+}
+input {
+  padding: 6px;
+}
+</style>
